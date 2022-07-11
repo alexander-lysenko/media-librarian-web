0cec90739bddd185bb966423b9050dcc556c5096
Alexander <linjay@ya.ru>
2022-07-11 17:47:04 +0300

    Added validation triggers

--- src/frontend/components/forms/SignupForm.tsx
+++ src/frontend/components/forms/SignupForm.tsx
@@ -11,17 +11,20 @@ import {
   SelectChangeEvent,
   TextField,
 } from "@mui/material";
-import React from "react";
+import React, { ChangeEvent, FocusEvent } from "react";
 
+import { useFormValidation } from "../../hooks/useFormValidation";
 import { useTranslation } from "../../hooks/useTranslation";
 import { Language } from "../../hooks/useTranslationStore";
 
-type Props = {
+type InputProps = {
   label: string;
   helperText?: string;
   name: string;
-  onChange: (event: SelectChangeEvent) => void;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (event: FocusEvent<HTMLInputElement>) => void;
   value?: string;
+  errorMessage: string | undefined;
 };
 
 /**
@@ -30,7 +33,8 @@ type Props = {
  */
 export const SignupForm = () => {
   const { t, getLanguage, setLanguage } = useTranslation();
-  // const formStore = {
+  const { values, errors, handleChange, handleBlur } = useFormValidation();
+  // const validationRules = {
   //   email: validation.required().string({min: 10}),
   //   username: validation.required().string(),
   //   password: validation.required().string(),
@@ -38,6 +42,7 @@ export const SignupForm = () => {
   //   language: validation.required().string(),
   // };
 
+  console.info(errors);
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -50,11 +55,11 @@ export const SignupForm = () => {
     });
   };
 
-  const handleLanguageSelect = (event: SelectChangeEvent) => {
+  const handleLanguageSelect = (event: ChangeEvent<HTMLInputElement>) => {
     setLanguage(event.target.value as Language);
   };
 
-  const handleThemeSelect = (event: SelectChangeEvent) => {
+  const handleThemeSelect = (event: ChangeEvent) => {
     console.log(event);
   };
 
@@ -63,17 +68,29 @@ export const SignupForm = () => {
       <UsernameTextField
         label={t("signupPage.username")}
         helperText={t("signupPage.usernameHint")}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        errorMessage={errors.username}
       />
-      <EmailTextField label={t("signupPage.email")} helperText={t("signupPage.emailAsLoginHint")} />
+      <EmailTextField
+        label={t("signupPage.email")}
+        helperText={t("signupPage.emailAsLoginHint")}
+        onChange={handleChange}
+        errorMessage={errors.email}
+      />
       <PasswordTextField
         name={"password"}
         label={t("signupPage.password")}
         helperText={t("signupPage.passwordHint")}
+        onChange={handleChange}
+        errorMessage={errors.password}
       />
       <PasswordTextField
         name={"passwordRepeat"}
         label={t("signupPage.passwordRepeat")}
         helperText={t("signupPage.passwordRepeatHint")}
+        onChange={handleChange}
+        errorMessage={errors.passwordRepeat}
       />
       <LanguageSelect
         onChange={handleLanguageSelect}
@@ -94,84 +111,97 @@ export const SignupForm = () => {
   );
 };
 
-const EmailTextField = ({ label, helperText }: Partial<Props>) => (
-  <TextField
-    size="small"
-    margin="dense"
-    required
-    fullWidth
-    id="email"
-    name="email"
-    label={label}
-    helperText={helperText}
-    autoComplete="email"
-    InputProps={{
-      endAdornment: (
-        <InputAdornment position="end">
-          <AlternateEmailOutlined />
-        </InputAdornment>
-      ),
-    }}
-  />
-);
+const UsernameTextField = (props: Partial<InputProps>) => {
+  return (
+    <TextField
+      size="small"
+      margin="dense"
+      required
+      fullWidth
+      id="username"
+      name="username"
+      label={props.label}
+      error={props.errorMessage !== undefined}
+      helperText={props.errorMessage || props.helperText}
+      autoComplete="name"
+      autoFocus
+      onChange={props.onChange}
+      onBlur={props.onBlur}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <BadgeOutlined />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
 
-const UsernameTextField = ({ label, helperText }: Partial<Props>) => (
-  <TextField
-    size="small"
-    margin="dense"
-    required
-    fullWidth
-    id="username"
-    name="username"
-    label={label}
-    helperText={helperText}
-    autoComplete="name"
-    autoFocus
-    InputProps={{
-      endAdornment: (
-        <InputAdornment position="end">
-          <BadgeOutlined />
-        </InputAdornment>
-      ),
-    }}
-  />
-);
+const EmailTextField = (props: Partial<InputProps>) => {
+  return (
+    <TextField
+      size="small"
+      margin="dense"
+      required
+      fullWidth
+      id="email"
+      name="email"
+      autoComplete="email"
+      label={props.label}
+      error={props.errorMessage !== undefined}
+      helperText={props.errorMessage || props.helperText}
+      onChange={props.onChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <AlternateEmailOutlined />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
 
-const PasswordTextField = ({ name, label, helperText }: Partial<Props>) => (
-  <TextField
-    size="small"
-    margin="dense"
-    required
-    fullWidth
-    type="password"
-    id={name}
-    name={name}
-    label={label}
-    helperText={helperText}
-    autoComplete="current-password"
-    InputProps={{
-      endAdornment: (
-        <InputAdornment position="end">
-          <PasswordOutlined />
-        </InputAdornment>
-      ),
-    }}
-  />
-);
+const PasswordTextField = (props: Partial<InputProps>) => {
+  return (
+    <TextField
+      size="small"
+      margin="dense"
+      required
+      fullWidth
+      type="password"
+      id={props.name}
+      name={props.name}
+      label={props.label}
+      error={props.errorMessage !== undefined}
+      helperText={props.errorMessage || props.helperText}
+      autoComplete="current-password"
+      onChange={props.onChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <PasswordOutlined />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
 
-const LanguageSelect = ({ onChange, label, value, helperText }: Partial<Props>) => {
+const LanguageSelect = (props: Partial<InputProps>) => {
   const { languages } = useTranslation();
   return (
     <FormControl fullWidth size="small" margin="dense">
-      <InputLabel id="language">{label}</InputLabel>
+      <InputLabel id="language">{props.label}</InputLabel>
       <Select
         required
         labelId="language"
         id="language"
         name="language"
-        value={value}
-        label={label}
-        onChange={onChange}
+        value={props.value}
+        label={props.label}
+        onChange={props.onChange as (event: SelectChangeEvent) => void | undefined}
       >
         {Object.entries(languages).map(([key, definition]) => (
           <MenuItem key={key} value={key}>
@@ -179,23 +209,23 @@ const LanguageSelect = ({ onChange, label, value, helperText }: Partial<Props>) 
           </MenuItem>
         ))}
       </Select>
-      <FormHelperText>{helperText}</FormHelperText>
+      <FormHelperText>{props.helperText}</FormHelperText>
     </FormControl>
   );
 };
 
-const ThemeSelect = ({ onChange, label, value, helperText }: Partial<Props>) => {
+const ThemeSelect = (props: Partial<InputProps>) => {
   return (
     <FormControl fullWidth size="small" margin="dense">
-      <InputLabel id="theme">{label}</InputLabel>
+      <InputLabel id="theme">{props.label}</InputLabel>
       <Select
         required
         labelId="theme"
         id="theme"
         name="theme"
-        value={value}
-        label={label}
-        onChange={onChange}
+        value={props.value}
+        label={props.label}
+        onChange={props.onChange as (event: SelectChangeEvent) => void | undefined}
       >
         <MenuItem key={"light"} value={"light"}>
           {"light"}
@@ -204,7 +234,7 @@ const ThemeSelect = ({ onChange, label, value, helperText }: Partial<Props>) => 
           {"dark"}
         </MenuItem>
       </Select>
-      <FormHelperText>{helperText}</FormHelperText>
+      <FormHelperText>{props.helperText}</FormHelperText>
     </FormControl>
   );
 };
--- src/frontend/hooks/useFormValidation.ts
+++ src/frontend/hooks/useFormValidation.ts
@@ -1,6 +1,50 @@
+import { ChangeEvent, FocusEvent, useRef, useState } from "react";
+
+import { Validator } from "../core/formValidation/validatior";
+
+type ValuesList = Record<string, unknown>;
+type RulesList = Record<keyof ValuesList, Validator[]>;
+type ErrorsList = Record<keyof ValuesList, string | undefined>;
+
 export const useFormValidation = () => {
-  // const formData = () => "";
-  // const validate = () => "";
-  // const errors = () => "";
-  return;
+  const [values, setValues] = useState({} as ValuesList);
+  const [rules, setRules] = useState({} as RulesList);
+  const [errors, setErrors] = useState({} as ErrorsList);
+
+  // Internal methods
+
+  const validate = (property: keyof ValuesList): void => {
+    const value = values[property];
+    console.log("Validate:", value);
+    // for (rule in rules[property]) {
+    //   if ()
+    // }
+    //
+    const errorMessage = value !== "" ? "validated" : undefined;
+    setErrors({ ...errors, [property]: errorMessage });
+  };
+
+  // Exportable methods
+
+  const setValidationRule = (property: string, ruleList: Validator[]) => {
+    setRules({ ...rules, [property]: ruleList });
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    console.log("change", { [event.target.name]: event.target.value });
+
+    setValues({ ...values, [event.target.name]: event.target.value });
+    validate(event.target.name);
+  };
+
+  const handleBlur = (event: FocusEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    console.log("blur", { [event.target.name]: event.target.value });
+
+    setValues({ ...values, [event.target.name]: event.target.value });
+    validate(event.target.name);
+  };
+
+  return { values, errors, setValidationRule, handleChange, handleBlur };
 };
