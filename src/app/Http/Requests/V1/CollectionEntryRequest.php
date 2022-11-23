00647f6fa146fdcd7cca8834e0534e394824954d
linjay <linjay@ya.ru>
2022-11-23 19:39:42 +0200

    Added CollectionEntryRequest, migration for jobs

---
+++ src/app/Http/Requests/V1/CollectionEntryRequest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use App\Models\SqliteCollectionMeta;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ * A request entity to validate the ID of an existing entry from an existing collection
+ * during view/update/delete the entry
+ * WIP
+ * @property int $id
+ * @property int $entry
+ */
+class CollectionEntryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    public function prepareForValidation(): void
+    {
+        $this->merge([
+            'id' => $this->route('id'),
+            'entry' => $this->route('entry'),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        $collectionMetaClass = SqliteCollectionMeta::class;
+        $collectionTableName = SqliteCollectionMeta::query()
+            ->where('id', '=', $this->id)
+            ->pluck('tbl_name')
+            ->first();
+
+        return [
+            'id' => ['required', 'integer', 'min:1', Rule::exists($collectionMetaClass, 'id')],
+            'entry' => ['required', 'integer', 'min:1', Rule::exists($collectionTableName, 'id')],
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'id.exists' => 'The collection with provided ID does not exist.',
+            'entry.exists' => 'The entry with provided ID does not exist.',
+        ];
+    }
+}
