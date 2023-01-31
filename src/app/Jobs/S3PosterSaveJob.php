eb21c2f1dfae794eb54b0aee8518fe1164680ee2
LinJay <linjay@ya.ru>
2023-01-31 18:16:48 +0200

    wip

--- src/app/Jobs/S3PosterSaveJob.php
+++ src/app/Jobs/S3PosterSaveJob.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class DeleteCollectionPosterJob implements ShouldQueue
+class S3PosterSaveJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
