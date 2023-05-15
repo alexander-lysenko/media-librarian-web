12c1e1e3ad87d564bf05b36b0e7f2e30219b9dcd
LinJay <linjay@ya.ru>
2023-05-15 11:47:55 +0300

    Updated docker-entrypoint.sh

--- docker/local/docker-entrypoint.sh
+++ docker/local/docker-entrypoint.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 DONE_STDOUT="\033[1;32m DONE \033[0m"
-WEB_UP_STDOUT="\033[1;32mWEB APP IS UP!\n\n \033[0m"
+WEB_UP_STDOUT="\033[1;32mWEB APP IS UP!\033[0m\n"
 
 function clean_webapp_logs() {
-  echo -n "Creating / Cleaning logs... "
+  echo "== Creating / Cleaning log files =="
   # Overwrites log files with an empty string
-  echo -n "" >/logs/nginx-error.log
-  echo -n "" >/logs/php-error.log
-  echo -n "" >/logs/php-fpm-access.log
-  echo -n "" >/logs/php-fpm-error.log
-  echo -n "" >/logs/supervisord.log
-  echo -n "" >/logs/xdebug.log
-  # Make an ability to read the log files outside the container
-  chown -R nginx:www-data /logs
+  echo -n "" >/logs/nginx-error.log && \
+  echo -e "Processing file: /logs/nginx-error.log ... $DONE_STDOUT"
+  echo -n "" >/logs/php-error.log && \
+  echo -e "Processing file: /logs/php-error.log ... $DONE_STDOUT"
+  echo -n "" >/logs/php-fpm-access.log && \
+  echo -e "Processing file: /logs/php-fpm-access.log ... $DONE_STDOUT"
+  echo -n "" >/logs/php-fpm-error.log && \
+  echo -e "Processing file: /logs/php-fpm-error.log ... $DONE_STDOUT"
+  echo -n "" >/logs/supervisord.log && \
+  echo -e "Processing file: /logs/supervisord.log ... $DONE_STDOUT"
+  echo -n "" >/logs/xdebug.log && \
+  echo -e "Processing file: /logs/xdebug.log ... $DONE_STDOUT"
 
+  # Make an ability to read the log files outside the container
+  chown -R nginx:www-data /logs && \
+  echo -e "Fixing permissions for root logs directory ... $DONE_STDOUT"
+
+  # Make an ability to read the log files outside the container
   if [ -f "/app/storage/logs/laravel.log" ]; then
-    chmod -R 777 /app/storage/logs/*
+    chmod -R 777 /app/storage/logs/* && \
+    echo -e "Fixing permissions for Laravel logs directory ... $DONE_STDOUT"
   fi
-  echo -e "$DONE_STDOUT"
 }
 
 function install_composer_dependencies() {
