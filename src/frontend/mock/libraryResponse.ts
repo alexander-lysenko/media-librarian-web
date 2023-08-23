9541479ca7e21b8d0a486f01713be5f68b7ce163
LinJay <linjay@ya.ru>
2023-08-23 17:56:15 +0300

    mocked libraryResponse

--- src/frontend/mock/libraryResponse.ts
+++ src/frontend/mock/libraryResponse.ts
@@ -8,6 +8,10 @@ type LibraryResponse = {
   };
 };
 
+type LibraryListResponse = {
+  data: LibrarySchema[];
+};
+
 export const libraryResponse: LibraryResponse = {
   data: {
     id: 1,
@@ -29,4 +33,38 @@ export const libraryResponse: LibraryResponse = {
     created_at: "1970-01-01 00:00:00",
     items_count: 1,
   },
+};
+
+export const librariesResponse: LibraryListResponse = {
+  data: [
+    {
+      id: 1,
+      title: "Movies",
+      fields: {
+        "Movie Title": "line",
+        "Origin Title": "line",
+        "Release Date": "date",
+        Description: "text",
+        "IMDB URL": "url",
+        "IMDB Rating": "rating10",
+        "My Rating": "rating5precision",
+        Watched: "checkmark",
+        "Watched At": "datetime",
+        "Chance to Advice": "priority",
+      },
+    },
+    {
+      id: 2,
+      title: "Music",
+      fields: {
+        Artist: "line",
+        Title: "line",
+        Album: "line",
+        Genre: "line",
+        Year: "date",
+        Rating: "rating5",
+        Lyrics: "text",
+      },
+    },
+  ],
 };
